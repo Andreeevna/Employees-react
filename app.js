8c86ac60dce2e9000f29ab3b6d017ddd33342141
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-12-19 19:52:51 +0300

    implemented controllers for registration and login

--- app.js
+++ app.js
@@ -14,6 +14,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.use('/users', usersRouter)
+app.use('/api/user', usersRouter)
 
 module.exports = app
