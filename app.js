282b54c0148fff9b2d7fa218b9027eab64443c50
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-12-28 17:55:03 +0300

    connecting RTK query

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
@@ -9,6 +10,7 @@ const usersRouter = require('./routes/users')
 
 const app = express()
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,20 +1,57 @@
 import { Card, Form, Row, Space, Typography } from 'antd'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserData, useLoginMutation } from '../../app/services/auth'
 import { CustomButton } from '../../components/custom-button'
 import { CustomInput } from '../../components/custom-input'
+import { ErrorMessage } from '../../components/error-message'
 import { Layout } from '../../components/layout'
 import { PasswordInput } from '../../components/password-input'
+import { selectUser } from '../../features/auth/authSlice'
 import { Paths } from '../../paths'
+import { isErrorWithMessage } from '../../utils/is-error-with-message'
 
 export const Login = () => {
+	const navigate = useNavigate()
+	const [error, setError] = useState('')
+	const user = useSelector(selectUser)
+	const [loginUser, loginUserResult] = useLoginMutation()
+
+	useEffect(() => {
+		if (user) {
+			navigate('/')
+		}
+	}, [user, navigate])
+
+	const login = async (data: UserData) => {
+		try {
+			await loginUser(data).unwrap()
+
+			navigate('/')
+		} catch (err) {
+			const maybeError = isErrorWithMessage(err)
+			console.log('err', err)
+			if (maybeError) {
+				setError(err.data.message)
+			} else {
+				setError('Неизвестная ошибка')
+			}
+		}
+	}
+
 	return (
 		<Layout>
 			<Row align='middle' justify='center'>
 				<Card title='Войдите' style={{ width: '30rem' }}>
-					<Form onFinish={() => {}}>
+					<Form onFinish={login}>
 						<CustomInput type='email' name='email' placeholder='Email' />
 						<PasswordInput name='password' placeholder='Пароль' />
-						<CustomButton type='primary' htmlType='submit'>
+						<CustomButton
+							type='primary'
+							htmlType='submit'
+							loading={loginUserResult.isLoading}
+						>
 							Войти
 						</CustomButton>
 					</Form>
@@ -22,6 +59,7 @@ export const Login = () => {
 						<Typography.Text>
 							Нет аккаунта? <Link to={Paths.register}>Зарегистрируйтесь</Link>
 						</Typography.Text>
+						<ErrorMessage message={error} />
 					</Space>
 				</Card>
 			</Row>
