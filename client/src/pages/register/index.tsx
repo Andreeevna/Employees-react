9a4a1d94e3590e50c4e084804ef7e9a9a708f50f
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-12-28 15:44:29 +0300

    feat: Implemented the registration component

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,3 +1,35 @@
+import { User } from '@prisma/client'
+import { Card, Form, Row, Space, Typography } from 'antd'
+import { Link } from 'react-router-dom'
+import { CustomButton } from '../../components/custom-button'
+import { CustomInput } from '../../components/custom-input'
+import { Layout } from '../../components/layout'
+import { PasswordInput } from '../../components/password-input'
+import { Paths } from '../../paths'
+
+type RegisterData = Omit<User, 'id'> & { confirmPassword: string }
+
 export const Register = () => {
-	return <div>register</div>
+	return (
+		<Layout>
+			<Row align='middle' justify='center'>
+				<Card title='Зарегистрируйтесь' style={{ width: '30rem' }}>
+					<Form onFinish={() => {}}>
+						<CustomInput type='text' name='name' placeholder='Имя' />
+						<CustomInput type='email' name='email' placeholder='Email' />
+						<PasswordInput name='password' placeholder='Пароль' />
+						<PasswordInput name='confirmPassword' placeholder='Пароль' />
+						<CustomButton type='primary' htmlType='submit'>
+							Зарегистрироваться
+						</CustomButton>
+					</Form>
+					<Space direction='vertical' size='large'>
+						<Typography.Text>
+							Уже зарегистрированы? <Link to={Paths.login}>Войдите</Link>
+						</Typography.Text>
+					</Space>
+				</Card>
+			</Row>
+		</Layout>
+	)
 }
