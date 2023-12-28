57dcf80bb473d87f577a23f576476618ed2c221a
Svetlana Andreevna <MS.Andreeevna@yandex.ru>
2023-12-28 15:35:25 +0300

    feat: Implemented the login component

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,10 +1,30 @@
-import { Row } from 'antd'
+import { Card, Form, Row, Space, Typography } from 'antd'
+import { Link } from 'react-router-dom'
+import { CustomButton } from '../../components/custom-button'
+import { CustomInput } from '../../components/custom-input'
 import { Layout } from '../../components/layout'
+import { PasswordInput } from '../../components/password-input'
+import { Paths } from '../../paths'
 
 export const Login = () => {
 	return (
 		<Layout>
-			<Row align='middle' justify='center'></Row>
+			<Row align='middle' justify='center'>
+				<Card title='Войдите' style={{ width: '30rem' }}>
+					<Form onFinish={() => {}}>
+						<CustomInput type='email' name='email' placeholder='Email' />
+						<PasswordInput name='password' placeholder='Пароль' />
+						<CustomButton type='primary' htmlType='submit'>
+							Войти
+						</CustomButton>
+					</Form>
+					<Space direction='vertical' size='large'>
+						<Typography.Text>
+							Нет аккаунта? <Link to={Paths.register}>Зарегистрируйтесь</Link>
+						</Typography.Text>
+					</Space>
+				</Card>
+			</Row>
 		</Layout>
 	)
 }
